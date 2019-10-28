8553a78ff6bc1ed94696b884dfbd0fc7dec9f5f8
obrezzzkova <obrezkova777@gmail.com>
2019-10-28 22:26:14 +0300

    a

--- librip/decorators.py
+++ librip/decorators.py
@@ -18,28 +18,3 @@ def print_result(func):
             print(func.__name__)
             print(result)
     return wrapper
-
-@print_result
-def test_1():
-    return 1
-
-
-@print_result
-def test_2():
-    return 'iu'
-
-
-@print_result
-def test_3():
-    return {'a': 1, 'b': 2}
-
-
-@print_result
-def test_4():
-    return [1, 2]
-
-
-test_1()
-test_2()
-test_3()
-test_4()
