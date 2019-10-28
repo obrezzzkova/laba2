6c674ea177075ebeba2fdb2b9fdbaa7d2cc7299f
obrezzzkova <obrezkova777@gmail.com>
2019-10-28 22:24:39 +0300

    a

--- librip/decorators.py
+++ librip/decorators.py
@@ -3,35 +3,43 @@
 # Если функция вернула список (list), то значения должны выводиться в столбик
 # Если функция вернула словарь (dict), то ключи и значения должны выводить в столбик через знак равно
 # Пример из ex_4.py:
-# @print_result
-# def test_1():
-#     return 1
-#
-# @print_result
-# def test_2():
-#     return 'iu'
-#
-# @print_result
-# def test_3():
-#     return {'a': 1, 'b': 2}
-#
-# @print_result
-# def test_4():
-#     return [1, 2]
-#
-# test_1()
-# test_2()
-# test_3()
-# test_4()
-#
-# На консоль выведется:
-# test_1
-# 1
-# test_2
-# iu
-# test_3
-# a = 1
-# b = 2
-# test_4
-# 1
-# 2
+
+def print_result(func):
+    def wrapper():
+        result = func()
+        if type(result) == list:
+            print(func.__name__)
+            for x in result: print(x)
+        elif type(result) == dict:
+            print(func.__name__)
+            for i in result:
+                print('{} = {}'.format(i, result[i]))
+        else:
+            print(func.__name__)
+            print(result)
+    return wrapper
+
+@print_result
+def test_1():
+    return 1
+
+
+@print_result
+def test_2():
+    return 'iu'
+
+
+@print_result
+def test_3():
+    return {'a': 1, 'b': 2}
+
+
+@print_result
+def test_4():
+    return [1, 2]
+
+
+test_1()
+test_2()
+test_3()
+test_4()
